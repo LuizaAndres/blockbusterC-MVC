a8ccfb220fa93b05c49997c1f7d79e668221d0a7
luiza <luiza_andres@uol.com.br>
2020-05-26 23:20:35 -0300

    listaFilmes works

--- Views/Filme/ListaFilmes.cs
+++ Views/Filme/ListaFilmes.cs
@@ -18,37 +18,40 @@ namespace Views
             this.BackColor = Color.Beige;
             this.Size = new Size(300,400);
 
-            lvFilmes = new ListView();
+             lvFilmes = new ListView();
             lvFilmes.Size = new Size(200,220);
-            lvFilmes.Location = new Point (20,30);
+            lvFilmes.Location = new Point(20,80);
             lvFilmes.View = Details;
-            ListViewItem Filmes = new ListViewItem();
-            foreach(Filme filme in FilmeController.GetFilmes()){
-                ListViewItem lvFilme = new ListViewItem(filme.NomeFilme);
+            ListViewItem filmes = new ListViewItem();
+            foreach (Filme filme in FilmeController.GetFilmes())
+            {
+                ListViewItem lvFilme = new ListViewItem(filme.FilmeId.ToString());
+                lvFilme.SubItems.Add(filme.NomeFilme);
                 lvFilme.SubItems.Add(filme.Valor.ToString());
-            } 
-            
+                lvFilmes.Items.Add(lvFilme);
+            }
+            lvFilmes.FullRowSelect = true;
+            lvFilmes.Columns.Add("ID", -2, HorizontalAlignment.Left);
             lvFilmes.Columns.Add("Nome", -2, HorizontalAlignment.Left);
             lvFilmes.Columns.Add("Valor", -2, HorizontalAlignment.Left);
             this.Controls.Add(lvFilmes);
 
             btnConfirma = new Button();
             btnConfirma.Size = new Size(80, 20);
-            btnConfirma.Location = new Point(20, 270);
+            btnConfirma.Location = new Point(20, 300);
             btnConfirma.Text = "Confirma";
             this.Controls.Add(btnConfirma);
             btnConfirma.Click += new EventHandler(btnConfirmaClick);
 
             btnCancela = new Button();
             btnCancela.Size = new Size(80, 20);
-            btnCancela.Location = new Point(120, 270);
+            btnCancela.Location = new Point(120, 300);
             btnCancela.Text = "Cancela";
             this.Controls.Add(btnCancela);
             btnCancela.Click += new EventHandler(btnCancelaClick);
         }
         private void btnConfirmaClick(object sender, EventArgs e)
         {
-            MessageBox.Show("Confirmado!!");
             parent.Show();
             this.Close();
         }
